0c236d37641767a185eded78fc432256ba6ddb1f
Noha Shehab <nohashehab.iti@gmail.com>
2025-08-10 11:37:09 +0300

    add urls file to the app

--- iti/iti/urls.py
+++ iti/iti/urls.py
@@ -15,14 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from home.views import test, user_home, user_profile
+from django.urls import path, include
+# from home.views import test, user_home, user_profile
 
+# best practice is to create urls file in each app folder
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test', test, name='testpage'),
-    path('<name>', user_home, name='user_home'),
-    # path('profile/<id>', user_profile, name='user_profile'),
-    path('profile/<int:id>', user_profile, name='user_profile') # limit the id to be an integer only
-
+    # path('test', test, name='testpage'),
+    # path('<name>', user_home, name='user_home'),
+    # # path('profile/<id>', user_profile, name='user_profile'),
+    # path('profile/<int:id>', user_profile, name='user_profile') # limit the id to be an integer only
+    path('home/', include('home.urls')),
 ]
