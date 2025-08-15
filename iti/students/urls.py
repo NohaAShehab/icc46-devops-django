44eddfc816d23981f6e00511539998e64b40b791
Noha Shehab <nohashehab.iti@gmail.com>
2025-08-15 12:03:02 +0300

    get objects via models, url names

--- iti/students/urls.py
+++ iti/students/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
-from students.views import students_list
+from students.views import students_list, all_students, show_student
 
 urlpatterns = [
-    path('list', students_list, name='students.list')
+    path('list', students_list, name='students.list'),
+    path('all', all_students, name='students.index'),
+    path('show/<int:id>', show_student, name='students.show')
 ]
--- iti/students/views.py
+++ iti/students/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from students.models import Student
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -17,7 +19,7 @@ def students_list(request):
         {'name': 'Jane', 'age': 21, 'id': 2},
         {'name': 'Jim', 'age': 22, 'id': 3},
     ]
-    return render(request, 'students/index.html', 
+    return render(request, 'students/staticIndex.html', 
     context={'students': students})
 
 
@@ -28,3 +30,43 @@ def students_list(request):
 # if condition, for loop, etc.
 
 
+
+"""
+( I need to get all students from the database )
+so I need to use Student model to get all students 
+
+models provides a way to interact with the database
+via functions  --> to deal with objects ?
+1- need to get all students ::: Student.objects.all() "select * from students;"
+# all query set of objects 
+
+2- need to get a student by id ::: 
+Student.objects.get(id=1) "select * from students where id=1;"
+get return with one 
+
+3- filter students by name ::: 
+Student.objects.filter(name="John") "select * from students where name='John';"
+filter return with list of objects 
+
+
+"""
+
+def all_students(request):
+    # 1- get all students from the database
+    students = Student.objects.all()
+    # 2- send the data to the template
+    return render(request, 'students/index.html', context={'students': students})
+
+
+
+def show_student(request, id):
+
+
+    """
+    if you want to get a student by id  if object not found 404 error will be raised
+    use get_object_or_404 function 
+    """
+    student = get_object_or_404(Student, id=id)
+    return render(request, 'students/show.html', 
+    context={'student': student})
+   
